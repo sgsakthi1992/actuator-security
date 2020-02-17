557c6501e09cf7889c93572b546fe409fffec58d
sgsakthi1992 <sgsakthi1992@gmail.com>
2020-02-17 15:17:35 +0100

    Initial commit

---
+++ src/main/java/com/sakthi/spring/actuatorsecurity/ActuatorSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.sakthi.spring.actuatorsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorSecurityApplication.class, args);
+	}
+
+}
